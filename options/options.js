4d141527d74bb4b4ed35610d72f6be2f440c2d91
Crowdbotics <team@crowdbotics.com>
2021-09-05 18:56:51 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "broken_bread_30332Identifier",
+  url: "https://broken_bread_30332Identifier.botics.co",
+  api: "https://broken_bread_30332Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
